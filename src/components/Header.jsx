bd9629b082d8a6776e7fd7d1b844d37afd7d039c
Ankush <ankushcuber14@gmail.com>
2024-07-02 11:43:11 +0530

    Header component was built

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,11 @@
+import { TbCheckbox } from 'react-icons/tb';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <TbCheckbox size={50} color="#b029ff" />
+      <h1>GET-IT-DONE</h1>
+    </div>
+  );
 };
 export default Header;
