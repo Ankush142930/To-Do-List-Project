3f3a3a30eddc64d591c96fa264d4045c504c48a6
Ankush <ankushcuber14@gmail.com>
2024-07-02 11:57:58 +0530

    TODOHero Component and Form Component were changed and elements were added and style.css file was also updated

--- src/components/TODOHero.jsx
+++ src/components/TODOHero.jsx
@@ -1,4 +1,15 @@
-const TODOHero = () => {
-  return <div>TODOHero</div>;
+/* eslint-disable react/prop-types */
+const TODOHero = ({ todos_completed, todos_total }) => {
+  return (
+    <section className="hero-container">
+      <div className="text-container">
+        <p className="large-text">Accomplished Tasks:</p>
+        <p>Push past your limits</p>
+      </div>
+      <div className="visual-container extra-large-text">
+        {todos_completed} / {todos_total}
+      </div>
+    </section>
+  );
 };
 export default TODOHero;
